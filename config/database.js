87ab816d206ecee6abf83494cd5a08a8ce9aa872
Uiliam Vogel <uiliam.vogel@bol.com.br>
2017-08-12 00:14:23 -0300

    atualizado mongoose

--- config/database.js
+++ config/database.js
@@ -8,20 +8,22 @@ mongoose.Promise = require('bluebird');
 
 const db = process.env.BANCO || "127.0.0.1:27017/medclick";
 const options = {
+  // useMongoClient:true,
   db : {safe : true},
   server : {
     auto_reconnect: true,
     poolSize: 5
   }
-}
+};
 
 function conectaDB(uri){
 
-  mongoose.connect('mongodb://'+uri,options);
+  mongoose.createConnection('mongodb://'+uri,options);
 
   mongoose.connection.on('connected', function(){
     console.log('Conectado ao mongodb em ' + uri);
   });
+
 
   mongoose.connection.on('error', function(err){
     console.log('Erro ao conectar ao mongo... Erro:'+  err);
@@ -35,7 +37,6 @@ function conectaDB(uri){
   mongoose.connection.on('open', function () {
     console.log('Conexão aberta mongoDB');
   });
-
 
   process.on('SIGINT', function () {
     mongoose.connection.close(function(){
